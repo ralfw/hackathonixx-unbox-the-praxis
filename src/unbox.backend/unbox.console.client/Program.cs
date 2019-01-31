b2740ee8451daec228d750763aadf3d7b13ef4b8
Ralf Westphal <info@ralfw.de>
2019-02-01 00:02:22 +0100

    console client begonnen

--- src/unbox.backend/unbox.console.client/Program.cs
+++ src/unbox.backend/unbox.console.client/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace unbox.console.client
+﻿using System.IO;
+using System.Linq;
+
+namespace unbox.console.client
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            if (args.Length > 0) {
+                var csvfilename = args[0];
+                var lines = File.ReadAllLines(csvfilename);
+                var records = lines.Skip(1).Select(line => line.Split(';'));
+                
+            }
         }
+        
+        
     }
 }
